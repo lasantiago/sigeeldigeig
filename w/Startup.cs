4e0fb7e0e72ed4b6081b5eb0f849e0e6af8663d2
Luis Santiago <luis.santiago@gmail.com>
2015-02-14 09:37:51 -0400

    Added the new solution type for w (MVC + Web API)

---
+++ w/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(w.Startup))]
+namespace w
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
